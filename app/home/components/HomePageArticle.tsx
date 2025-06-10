ab7c306572f44f345ef193fd4eb74d5422673372
Octavio Zenil <octaviozenil@gmail.com>
2025-06-10 10:57:48 -0700

    Correcting HomePageArticle

--- app/home/components/HomePageArticle.tsx
+++ app/home/components/HomePageArticle.tsx
@@ -45,12 +45,18 @@ export function HomePageArticle({ title, content, Icon }: HomePageArticle) {
    else {
     icon = Icon
   }
-  
+
+  let parragraph
+  if (typeof content === 'string'){
+    parragraph = <p className="text-center text-sm font-light" dangerouslySetInnerHTML={{__html: content}}></p>
+  } else {
+    parragraph = <p className="text-center text-sm font-light">{content}</p>
+  }
   return (
     <article className="flex flex-col items-center gap-2">
       <RoundedIcon Icon={icon} />
       <h3 className="font-semibold">{title}</h3>
-      <p className="text-center text-sm font-light" dangerouslySetInnerHTML={{__html: content}}></p>
+          {parragraph}
     </article>
   )
 }
